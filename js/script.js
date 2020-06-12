878000c877e885095ab7947af0ac9929258389e8
demetrinh <demetrinh@gmail.com>
2020-06-11 20:30:37 -0400

    Deliverable 2

---
+++ js/script.js
@@ -0,0 +1,87 @@
+var vacationType = prompt ("What kind of trip would you like to go on, musical, tropical, or adventurous?");
+console.log(vacationType); {
+
+if (vacationType === "musical") {
+    var groupSize = prompt ("How many people are in your group?" )
+    console.log(groupSize);
+    
+        if (groupSize === "1" && vacationType === "musical") {
+            var result = console.log("Since you're a group of 1 going on a musical vacation, you should take a first class flight to New Orleans.");
+             
+            
+        } 
+
+
+        if (groupSize ==="2" && vacationType === "musical") {
+            var result = console.log("Since you're a group of 2 going on a musical vacation, you should take a first class flight to New Orleans.");
+        } 
+        if (groupSize ==="3" && vacationType === "musical") {
+            var result = console.log("Since you're a group of 3 going on a musical vacation, you should take a helicopter to New Orleans.");
+        } 
+        if (groupSize ==="4" && vacationType === "musical") {
+            var result = console.log("Since you're a group of 4 going on a musical vacation, you should take a helicopter to New Orleans.");
+        } 
+        if (groupSize ==="5" && vacationType === "musical") {
+            var result = console.log("Since you're a group of 5 going on a musical vacation, you should take a helicopter to New Orleans.");
+        } 
+        if (groupSize >="6" && vacationType === "musical") {
+            var result = console.log("Since you're a group of 6+ going on a musical vacation, you should take a charter flight to New Orleans.");
+        } 
+} 
+if (vacationType === "tropical") {
+    var groupSize = prompt ("How many people are in your group?")
+    console.log(groupSize);
+        if (groupSize === "1" && vacationType === "tropical") {
+            var result = console.log("Since you're a group of 1 going on a tropical vacation, you should take a first class flight to a beach vacation in Mexico");
+        }
+}
+        if (groupSize ==="2" && vacationType === "tropical") {
+            var result = console.log("Since you're a group of 2 going on a tropical vacation, you should take a first class flight to a beach vacation in Mexico");
+}
+        if (groupSize ==="3" && vacationType === "tropical") {
+            var result = console.log("Since you're a group of 3 going on a tropical vacation, you should take a helicopter to a beach vacation in Mexico");
+}
+        if (groupSize ==="4" && vacationType === "tropical") {
+            var result = console.log("Since you're a group of 4 going on a tropical vacation, you should take a helicopter to a beach vacation in Mexico");
+}
+        if (groupSize ==="5" && vacationType === "tropical") {
+            var result = console.log("Since you're a group of 5 going on a tropical vacation, you should take a helicopter to a beach vacation in Mexico");
+}
+        if (groupSize >="6" && vacationType === "tropical") {
+            var result = console.log("Since you're a group of 6+ going on a tropical vacation, you should take a charter flight to a beach vacation in Mexico");
+}
+if (vacationType === "adventurous") {
+    var groupSize = prompt ("How many people are in your group?")
+    console.log(groupSize);
+        if (groupSize === "1" && vacationType === "adventurous") {
+            var result = console.log("Since you're a group of 1 going on a adventurous vacation, you should take a first class flight to go Whitewater Rafting the Grand Canyon");
+        }
+}
+        if (groupSize ==="2" && vacationType === "adventurous") {
+            var result = console.log("Since you're a group of 2 going on a adventurous vacation, you should take a first class flight to go Whitewater Rafting the Grand Canyon");
+}
+        if (groupSize ==="3" && vacationType === "adventurous") {
+            var result = console.log("Since you're a group of 3 going on a adventurous vacation, you should take a helicopter to go Whitewater Rafting the Grand Canyon");
+}
+        if (groupSize ==="4" && vacationType === "adventurous") {
+            var result = console.log("Since you're a group of 4 going on a adventurous vacation, you should take a helicopter to go Whitewater Rafting the Grand Canyon");
+}
+        if (groupSize ==="5" && vacationType === "adventurous") {
+            var result = console.log("Since you're a group of 5 going on a adventurous vacation, you should take a helicopter to go Whitewater Rafting the Grand Canyon");
+}
+        if (groupSize >="6" && vacationType === "adventurous") {
+            var result = console.log("Since you're a group of 6+ going on a adventurous vacation, you should take a charter flight go Whitewater Rafting the Grand Canyon");
+}
+
+
+
+
+}
+
+
+
+
+
+
+
+
